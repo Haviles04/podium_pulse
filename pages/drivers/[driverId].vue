2a27406c6cb0a4b08d8b3e076f421154a0237e6f
Henry Aviles <haviles04@gmail.com>
2023-08-03 13:38:52 -0400

    feat: added driver page

---
+++ pages/drivers/[driverId].vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    <p>{{ driver.givenName + " " + driver.familyName }}</p>
+    <p>{{ driver.dateOfBirth }}</p>
+    <p>{{ driver.permanentNumber }}</p>
+    <p>{{ driver.nationality }}</p>
+  </div>
+</template>
+
+<script setup>
+const { driverId } = useRoute().params;
+
+const { data: driverData } = await useFetch(
+  `http://ergast.com/api/f1/drivers/${driverId}.json`
+);
+
+const [driver] = driverData.value.MRData.DriverTable.Drivers;
+console.log(driver);
+</script>
