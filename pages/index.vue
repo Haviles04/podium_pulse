db88794ba88fe83cc3ab04b58ee2b73b11e211ab
Henry Aviles <haviles04@gmail.com>
2023-08-06 14:46:36 -0400

    chore: removed unneccesary class

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="hero flex justify-center items-center flex-col pt-20 font-racing italic h-screen bg-[url('./public/f1.jpg')]]"
+    class="hero flex justify-center items-center flex-col pt-20 font-racing italic h-screen]"
   >
     <h1 class="text-8xl text-primary bg-slate-500 rounded-xl p-4">
       Podium Pulse
