fdd974aaea6578eaaf43aba1b42e72226f9b19e2
Henry Aviles <haviles04@gmail.com>
2023-08-26 15:04:15 -0400

    feat: removed unneccesary api call

--- pages/[seasonId]/round/[roundId].vue
+++ pages/[seasonId]/round/[roundId].vue
@@ -1,8 +1,10 @@
 <template>
   <section>
     <div class="text-center pt-8 [&>*]:p-4 [&>h2]:text-2xl [&>h3]:text-xl">
-      <h1 class="text-4xl font-racing">{{ roundInfo.raceName }}</h1>
-      <h2 class="text-2xl font-racing">{{ roundInfo.Circuit.circuitName }}</h2>
+      <h1 class="text-4xl font-racing">{{ sessionInfo.raceName }}</h1>
+      <h2 class="text-2xl font-racing">
+        {{ sessionInfo.Circuit.circuitName }}
+      </h2>
 
       <select class="text-center text-black" v-model="sessionType">
         <option disabled default value="">Select Session</option>
@@ -18,15 +20,11 @@
 
 <script setup>
 const { seasonId, roundId } = useRoute().params;
-const sessionType = ref("quali");
+const sessionType = ref("Race");
 
 const sessionInfo = computed(() => {
   return sessionType.value === "Race" ? raceInfo : qualiInfo;
 });
-
-const roundInfo = await fetchSessionInfo(
-  `http://ergast.com/api/f1/${seasonId}/${roundId}.json`
-);
 
 const qualiInfo = await fetchSessionInfo(
   `http://ergast.com/api/f1/${seasonId}/${roundId}/qualifying.json`
