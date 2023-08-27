8247350d11ff096cdccd360ef62ab0ec99dacecb
Henry Aviles <haviles04@gmail.com>
2023-08-27 13:14:15 -0400

    change: changed from select to radio

--- pages/[seasonId]/round/[roundId].vue
+++ pages/[seasonId]/round/[roundId].vue
@@ -1,21 +1,44 @@
 <template>
   <section>
     <div class="text-center pt-8 [&>*]:p-4 [&>h2]:text-2xl [&>h3]:text-xl">
-      <h1 class="text-4xl font-racing">{{ raceInfo.raceName }}</h1>
+      <h1 class="text-4xl font-racing">{{ roundInfo.raceName }}</h1>
       <h2 class="text-2xl font-racing">
-        {{ raceInfo.Circuit.circuitName }}
+        {{ roundInfo.Circuit.circuitName }}
       </h2>
 
-      <select class="text-center text-black" v-model="sessionType">
-        <option disabled default value="">Select Session</option>
-        <option value="Qualifying">Qualifying</option>
-        <option value="Race">Race</option>
-      </select>
+      <form>
+        <input
+          class="hidden peer/race"
+          type="radio"
+          id="race"
+          value="Race"
+          v-model="sessionType"
+        />
+        <label
+          for="race"
+          class="bg-slate-500 p-4 text-xl m-4 rounded-xl peer-checked/race:bg-white peer-checked/race:text-slate-500 cursor-pointer"
+        >
+          Race
+        </label>
 
-      <h3>{{ sessionType }}</h3>
+        <input
+          class="hidden peer/quali"
+          type="radio"
+          id="quali"
+          value="Qualifying"
+          v-model="sessionType"
+        />
+        <label
+          for="quali"
+          class="bg-slate-500 p-4 text-xl m-4 rounded-xl peer-checked/quali:bg-white peer-checked/quali:text-slate-500 cursor-pointer"
+        >
+          Qualifying</label
+        >
+      </form>
+
       <RaceResultsTable
         :results="sessionInfo"
-        :key="sessionInfo.raceName + sessionType"
+        :key="roundInfo.raceName + sessionType"
       />
     </div>
   </section>
@@ -29,6 +52,10 @@ const sessionInfo = computed(() => {
   return sessionType.value === "Race" ? raceInfo : qualiInfo || null;
 });
 
+const roundInfo = await fetchSessionInfo(
+  `http://ergast.com/api/f1/${seasonId}/${roundId}.json`
+);
+
 const qualiInfo = await fetchSessionInfo(
   `http://ergast.com/api/f1/${seasonId}/${roundId}/qualifying.json`
 );
@@ -36,6 +63,4 @@ const qualiInfo = await fetchSessionInfo(
 const raceInfo = await fetchSessionInfo(
   `http://ergast.com/api/f1/${seasonId}/${roundId}/results.json`
 );
-
-console.log(sessionInfo.value);
 </script>
