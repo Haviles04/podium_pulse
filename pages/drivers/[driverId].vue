18250df415843a68db7c39948b05d4faca7e5530
Henry Aviles <haviles04@gmail.com>
2023-09-01 17:02:47 -0400

    fix: semantics

--- pages/drivers/[driverId].vue
+++ pages/drivers/[driverId].vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="text-center">
-    <h2 class="text-6xl font-racing my-6">Driver Profile</h2>
-    <p class="text-2xl">{{ driver.givenName + " " + driver.familyName }}</p>
-    <img
-      class="inline ml-2"
-      :src="`https://flagsapi.com/${countryCode}/flat/48.png`"
-    />
-    <p>Nationality: {{ driver.nationality }}</p>
-    <p>DOB: {{ driver.dateOfBirth }}</p>
-    <p>Number: {{ driver.permanentNumber }}</p>
-  </div>
+  <section>
+    <div class="text-center">
+      <h2 class="text-6xl font-racing my-6">Driver Profile</h2>
+      <p class="text-2xl">{{ driver.givenName + " " + driver.familyName }}</p>
+      <img
+        class="inline ml-2"
+        :src="`https://flagsapi.com/${countryCode}/flat/48.png`"
+      />
+      <p>Nationality: {{ driver.nationality }}</p>
+      <p>DOB: {{ driver.dateOfBirth }}</p>
+      <p>Number: {{ driver.permanentNumber }}</p>
+    </div>
+  </section>
 </template>
 
 <script setup>
