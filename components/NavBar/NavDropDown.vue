9cef313f68c2802c558dfd10a8e2bf07d24ad09e
Henry Aviles <haviles04@gmail.com>
2023-09-02 18:08:44 -0400

    moved dropdown to seperate component

---
+++ components/NavBar/NavDropDown.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="absolute left-0 w-full bg-background px-8 py-6 rounded-xl min-w-[200px] text-center -z-10"
+  >
+    <ul>
+      <li v-for="data in listData" class="list-none">
+        <nuxt-link :to="listLink(data)">
+          {{ data.season || data.givenName + " " + data?.familyName }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const { listData, listTypes } = defineProps(["listData", "listTypes"]);
+
+const listLink = (data) => {
+  if (listTypes === "seasons") {
+    return { path: `/seasons/${data?.season}` };
+  }
+  return { path: `/drivers/${data?.driverId}` };
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,34 +2,26 @@
   <nav
     class="min-h-[50px] flex items-center justify-between border-b-2 border-primary"
   >
-    <p class="mx-6"><nuxt-link to="/">Podium Pulse</nuxt-link></p>
-    <div class="hidden md:block flex items-center z-10 relative">
-      <nuxt-link to="/schedule" class="mx-8">Schedule</nuxt-link>
-      <span @mouseleave="listTypes = ''">
-        <button class="mx-8" @mouseenter="setSessionType('seasons')">
-          <nuxt-link to="/seasons"> Seasons </nuxt-link>
-        </button>
-        <button class="mx-8" @mouseenter="setSessionType('drivers')">
-          <nuxt-link to="/drivers">Drivers</nuxt-link>
-        </button>
-        <div
-          v-if="showList"
-          class="absolute top-1 pt-10 right-6 bg-background px-8 py-2 rounded-xl min-w-[200px] text-center -z-10"
-        >
-          <ul>
-            <li
-              v-for="data in listData"
-              class="list-none"
-              @click="listTypes = ''"
-            >
-              <nuxt-link :to="listLink(data)">
-                {{ data.season || data.givenName + " " + data?.familyName }}
-              </nuxt-link>
-            </li>
-          </ul>
-        </div>
-      </span>
-    </div>
+    <nuxt-link to="/" class="mx-6">Podium Pulse</nuxt-link>
+    <ul class="hidden md:flex items-center z-10 relative">
+      <li><nuxt-link to="/schedule" class="mx-8">Schedule</nuxt-link></li>
+      <li class="mx-8" @mouseenter="setSessionType('seasons')">
+        <nuxt-link to="/seasons"> Seasons </nuxt-link>
+        <nav-drop-down
+          v-if="listTypes === 'seasons'"
+          :listData="seasons"
+          :listTypes="listTypes"
+        />
+      </li>
+      <li class="mx-8" @mouseenter="setSessionType('drivers')">
+        <nuxt-link to="/drivers">Drivers</nuxt-link>
+        <nav-drop-down
+          v-if="listTypes === 'drivers'"
+          :listData="drivers"
+          :listTypes="listTypes"
+        />
+      </li>
+    </ul>
     <div class="block md:hidden">
       <button class="mx-8">Menu</button>
     </div>
@@ -46,21 +38,6 @@ const setSessionType = (type) => {
   sessionTypeTimer = setTimeout(() => {
     listTypes.value = type;
   }, 250);
-};
-
-const showList = computed(() => {
-  return listTypes.value === "drivers" || listTypes.value === "seasons";
-});
-
-const listData = computed(() => {
-  return listTypes.value === "seasons" ? seasons : drivers;
-});
-
-const listLink = (data) => {
-  if (listTypes.value === "seasons") {
-    return { path: `/seasons/${data?.season}` };
-  }
-  return { path: `/drivers/${data?.driverId}` };
 };
 
 //Get Season data
