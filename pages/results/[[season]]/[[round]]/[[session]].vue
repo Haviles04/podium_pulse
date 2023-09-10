bcc813e7142b7613eb809a986883a03ea2ab6280
Henry Aviles <haviles04@gmail.com>
2023-09-10 11:30:52 -0400

    feat: updated session

--- pages/results/[[season]]/[[round]]/[[session]].vue
+++ pages/results/[[season]]/[[round]]/[[session]].vue
@@ -2,10 +2,13 @@
   <main>
     <section class="text-center">
       <result-select />
-      <div v-if="showOnlySeason">Season</div>
+      <div v-if="showOnlySeason">NEED TO MAKE SEASON TABLE</div>
       <div v-else>
-        <h1 class="m-10 text-6xl font-racing">{{ race.raceName }}</h1>
-        <race-results-table :results="race" :sessionType="session" />
+        <h1 class="m-10 text-6xl font-racing">{{ raceData.raceName }}</h1>
+        <race-results-table
+          :results="raceData"
+          :sessionType="session || 'race'"
+        />
       </div>
     </section>
   </main>
@@ -14,18 +17,22 @@
 <script setup>
 const route = useRoute();
 const { season, round, session } = route.params;
-const loading = ref(false);
 const showOnlySeason = season && !round && !session;
+let seasonData;
+let raceData;
 
 // SSR
-let race =
-  !season && !round && !session
-    ? await fetchSessionInfo(
-        "http://ergast.com/api/f1/current/last/results.json"
-      )
-    : await fetchSessionInfo(
-        `http://ergast.com/api/f1/${season}/${round}/${
-          session === "race" ? "results" : "qualifying"
-        }.json`
-      );
+
+if (showOnlySeason) {
+  await useFetch(`http://ergast.com/api/f1/${season}.json`).then(
+    ({ data }) => (seasonData = data.value.MRData.RaceTable.Races)
+  );
+} else {
+  const sessionType = session === "qualifying" ? session : "results";
+  await useFetch(
+    `http://ergast.com/api/f1/${season}/${round}/${sessionType}.json`
+  ).then(({ data }) => {
+    raceData = data.value.MRData.RaceTable.Races[0];
+  });
+}
 </script>
