a9ebd1c4c7bee9c59398bd49580a8d440d750224
Henry Aviles <haviles04@gmail.com>
2023-09-10 11:13:12 -0400

    fix: changed the way route is selected and pushed

--- components/RaceResults/ResultSelect.vue
+++ components/RaceResults/ResultSelect.vue
@@ -1,39 +1,87 @@
 <template>
-  <label class="text-primary" :for="label"
-    ><span class="text-xl underline">{{ label }}</span>
-    <select
-      v-model="value"
-      :name="label"
-      :id="label + 'select'"
-      size="5"
-      class="bg-background text-primary block no-scrollbar m-1 border rounded-xl p-4 border-light"
-      @change="handleChange"
-    >
-      <option v-for="item in items" :value="item.round ?? item">
-        {{ item.raceName ?? item }}
-      </option>
-    </select>
-  </label>
+  <div
+    v-if="loading"
+    class="absolute flex justify-center items-center w-full h-[calc(100vh-60px)] bg-background opacity-50"
+  >
+    <Loader />
+  </div>
+  <form class="pt-10 [&>*]:m-2 flex justify-center items-start">
+    <label class="text-primary" for="year"
+      ><span class="text-2xl text-light">Year</span>
+      <select
+        v-model="selectedSeason"
+        name="year"
+        id="yearSelect"
+        size="5"
+        class="bg-background text-primary block no-scrollbar m-1 border-2 rounded-xl p-4 border-light"
+        @change="handleSeasonChange"
+      >
+        <option v-for="year in years" :value="year">{{ year }}</option>
+      </select>
+    </label>
+    <label class="text-primary" for="round"
+      ><span class="text-2xl text-light">Round</span>
+      <select
+        v-model="selectedRound"
+        name="round"
+        id="roundSelect"
+        size="5"
+        class="bg-background text-primary block no-scrollbar m-1 border-2 rounded-xl p-4 border-light"
+        @change="handleRoundChange"
+      >
+        <option v-for="race in raceList" :value="race.round">
+          {{ race.raceName }}
+        </option>
+      </select>
+    </label>
+    <label class="text-primary" for="session"
+      ><span class="text-2xl text-light">Session</span>
+      <select
+        v-model="selectedSession"
+        name="session"
+        id="sessionSelect"
+        size="5"
+        class="bg-background text-primary block no-scrollbar m-1 border-2 rounded-xl p-4 border-light"
+        @change="handleRoundChange"
+      >
+        <option value="race">Race</option>
+        <option value="qualifying">Qualifying</option>
+      </select>
+    </label>
+  </form>
 </template>
 
 <script setup>
-const { modelValue, label, items } = defineProps([
-  "modelValue",
-  "label",
-  "items",
-]);
-const emit = defineEmits(["update:modelValue", "handleResultsChange"]);
+const router = useRouter();
+const { season, round, session } = useRoute().params;
+const years = getLast10Seasons();
+const selectedSeason = ref(season);
+const selectedRound = ref(round);
+const selectedSession = ref(session);
+const loading = ref(false);
 
-const value = computed({
-  get() {
-    return modelValue;
-  },
-  set(value) {
-    emit("update:modelValue", value);
-  },
-});
-
-const handleChange = () => {
-  emit("handleResultsChange");
+const handleSeasonChange = () => {
+  loading.value = true;
+  router.push({
+    path: `/results/${selectedSeason.value}`,
+  });
 };
+
+const handleRoundChange = () => {
+  loading.value = true;
+  router.push({
+    path: `/results/${selectedSeason.value}/${selectedRound.value}/${
+      selectedSession.value || "race"
+    }`,
+  });
+};
+
+// SSR
+const { data: seasonData } = await useFetch(
+  `http://ergast.com/api/f1/${season}.json`
+);
+const seasonRaces = seasonData.value.MRData.RaceTable.Races;
+const raceList = seasonRaces
+  .filter(({ date }) => Date.now() >= Date.parse(date))
+  .map(({ raceName, round }) => ({ raceName, round }));
 </script>
--- pages/results/[[season]]/[[round]]/[[session]].vue
+++ pages/results/[[season]]/[[round]]/[[session]].vue
@@ -1,63 +1,23 @@
 <template>
   <main>
-    <div
-      v-if="loading"
-      class="absolute flex justify-center items-center w-screen h-screen bg-background opacity-50"
-    >
-      <Loader />
-    </div>
     <section class="text-center">
-      <form class="pt-10 [&>*]:m-6 flex justify-center align-center">
-        <result-select
-          v-model="selectedSeason"
-          :items="years"
-          label="Season"
-          @handleResultsChange="handleSeasonChange"
-        />
-        <result-select
-          v-model="selectedRound"
-          :items="raceList"
-          label="Round"
-          @handleResultsChange="handleRoundChange"
-        />
-        <result-select
-          v-model="selectedSession"
-          :items="['race', 'qualifying']"
-          label="Round"
-          @handleResultsChange="handleRoundChange"
-        />
-      </form>
-      <h1 class="m-10 text-6xl font-racing">{{ race.raceName }}</h1>
-      <race-results-table :results="race" :sessionType="selectedSession" />
+      <result-select />
+      <div v-if="showOnlySeason">Season</div>
+      <div v-else>
+        <h1 class="m-10 text-6xl font-racing">{{ race.raceName }}</h1>
+        <race-results-table :results="race" :sessionType="session" />
+      </div>
     </section>
   </main>
 </template>
 
 <script setup>
-const loading = ref(false);
 const route = useRoute();
-const router = useRouter();
 const { season, round, session } = route.params;
-const selectedSeason = ref(season);
-const selectedRound = ref(round);
-const selectedSession = ref(session || "race");
-const years = getLast10Seasons();
+const loading = ref(false);
+const showOnlySeason = season && !round && !session;
 
-const handleSeasonChange = () => {
-  loading.value = true;
-  router.push({
-    path: `/results/${selectedSeason.value}`,
-  });
-};
-
-const handleRoundChange = () => {
-  loading.value = true;
-  router.push({
-    path: `/results/${selectedSeason.value}/${selectedRound.value}/${selectedSession.value}`,
-  });
-};
-
-//SSR//
+// SSR
 let race =
   !season && !round && !session
     ? await fetchSessionInfo(
@@ -68,15 +28,4 @@ let race =
           session === "race" ? "results" : "qualifying"
         }.json`
       );
-selectedSeason.value = race.season;
-selectedRound.value = race.round;
-
-const { data: seasonData } = await useFetch(
-  `http://ergast.com/api/f1/${selectedSeason.value}.json`
-);
-const seasonRaces = seasonData.value.MRData.RaceTable.Races;
-const raceList = seasonRaces.map(({ raceName, round }) => ({
-  raceName,
-  round,
-}));
 </script>
