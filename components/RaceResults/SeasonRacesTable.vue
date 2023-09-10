066719c9a7063117c60e984a9a330e8355b9cc36
Henry Aviles <haviles04@gmail.com>
2023-09-10 13:18:09 -0400

    fix: semantics

--- components/RaceResults/SeasonRacesTable.vue
+++ components/RaceResults/SeasonRacesTable.vue
@@ -1,19 +1,21 @@
 <template>
-  <table
-    class="table-auto border-2 min-w-[600px] divide-y divide-primary rounded m-auto text-center"
-  >
-    <thead>
-      <tr>
-        <th>Round</th>
-        <th>Race</th>
-        <th>Date</th>
-        <th>Time</th>
-      </tr>
-    </thead>
-    <tbody class="divide-y divide-dashed divide-primary">
-      <season-table-row v-for="race in races" :race="race" />
-    </tbody>
-  </table>
+  <section>
+    <table
+      class="table-auto border-2 min-w-[600px] divide-y divide-primary m-auto text-center"
+    >
+      <thead>
+        <tr>
+          <th>Round</th>
+          <th>Race</th>
+          <th>Date</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-dashed divide-primary">
+        <season-table-row v-for="race in races" :race="race" />
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script setup>
