21a8924a9237372af819f592176869e619fde6f5
Henry Aviles <haviles04@gmail.com>
2023-09-25 16:47:12 -0400

    feat: updated useNewsFetch

--- composables/useNews.js
+++ composables/useNews.js
@@ -4,7 +4,7 @@ export const useNews = () => {
   const yesterday = new Date(Date.now() - 86400000).toLocaleDateString();
   if (!news.value) {
     const { data } = useFetch(
-      `https://newsapi.org/v2/everything?language=en&apiKey=${config.public.newsApiKey}&q=F1 Race&from=${yesterday}`,
+      `https://newsapi.org/v2/everything?language=en&apiKey=${config.public.newsApiKey}&q=F1 Race&from=${yesterday}&domains=autosport.com, bbc.co.uk, sbnation.com, skysports.com`,
       {
         transform: (data) => {
           return data.articles.filter(({ title }) => title !== '[Removed]').slice(0, 5);
