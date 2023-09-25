c1bbe6fed5d13b17b32145fce0868de7437f5227
Henry Aviles <haviles04@gmail.com>
2023-09-25 15:22:24 -0400

    devEx: added material icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
-    cssPath: "~/assets/css/tailwind.css",
+    cssPath: '~/assets/css/tailwind.css',
   },
   components: [
     {
-      path: "~/components",
+      path: '~/components',
       pathPrefix: false,
     },
   ],
+  app: {
+    head: {
+      title: 'Podium Pulse | F1',
+      meta: [{ name: 'description', content: 'Everything about formula 1' }],
+      link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }],
+    },
+  },
 });
