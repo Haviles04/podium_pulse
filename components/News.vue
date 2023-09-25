68a41ae23df371bb32d1cbc69b2d0fbe839b9215
Henry Aviles <haviles04@gmail.com>
2023-09-24 20:23:11 -0400

    feat: added new section/changed colors

---
+++ components/News.vue
@@ -0,0 +1,38 @@
+<template>
+  <section
+    class="grid grid-cols-1 md:grid-cols-2 m-auto gap-10 mt-20 max-w-[1000px] bg-secondary p-8 rounded-xl"
+  >
+    <article
+      v-for="(article, i) in news"
+      :class="`text-centerflex flex-col rounded-xl shadow shadow-light overflow-hidden ${
+        i === 0 ? 'md:col-span-2' : null
+      }`"
+    >
+      <img class="w-full" :src="article.urlToImage" />
+      <h3 class="text-xl text-center m-2">
+        <a
+          :href="article.url"
+          target="_blank"
+          class="flex flex-col justify-between"
+          >{{ article.title }}</a
+        >
+      </h3>
+
+      <p class="text-center p-2">{{ article.description }}</p>
+    </article>
+  </section>
+</template>
+
+<script setup>
+const yesterday = new Date(Date.now() - 86400000).toLocaleDateString();
+const { data: news } = await useFetch(
+  `https://newsapi.org/v2/everything?language=en&apiKey=061e9934238d4732bcfcd5d21402ecec&q=F1 Race&from=${yesterday}`,
+  {
+    transform: (data) => {
+      return data.articles
+        .filter(({ title }) => title !== "[Removed]")
+        .slice(0, 5);
+    },
+  }
+);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,37 @@
 <template>
-  <div
-    class="bg-[url('/f1.jpg')] bg-no-repeat bg-center bg-fixed flex justify-center items-center flex-col pt-20 font-racing italic h-[75vh] md:h-[50vh] text-center"
-  >
-    <h1 class="text-6xl md:text-8xl bg-background rounded-xl p-4">
-      Podium Pulse
-    </h1>
-    <p
-      class="text-2xl md:text-4xl bg-background border-b-2 border-primary rounded-xl p-4"
+  <main>
+    <div
+      class="bg-[url('/f1.jpg')] bg-no-repeat bg-center bg-fixed flex justify-center items-center flex-col pt-20 font-racing italic h-[75vh] md:h-[50vh] text-center"
     >
-      Fueling Your F1 Passion!
-    </p>
-  </div>
-  <h2
-    class="text-4xl md:text-7xl text-center text-bold italic font-racing mt-10 border-b-2 border-primary w-fit m-auto"
-  >
-    Last Race
-  </h2>
-  <h3
-    class="text-2xl md:text-4xl text-center text-bold italic font-racing m-10"
-  >
-    {{ lastRace.raceName }}
-  </h3>
-  <race-results-table :results="lastRace" sessionType="race" />
+      <h1 class="text-6xl md:text-8xl bg-secondary rounded-xl p-4">
+        Podium Pulse
+      </h1>
+      <p
+        class="text-2xl md:text-4xl bg-secondary border-b-2 border-primary rounded-xl p-4"
+      >
+        Fueling Your F1 Passion!
+      </p>
+    </div>
+    <h2
+      class="text-center text-4xl md:text-7xl font-racing italic text-bold mt-6"
+    >
+      Recent News
+    </h2>
+    <News />
+    <div class="bg-secondary mt-10 p-2">
+      <h2
+        class="text-4xl md:text-7xl text-center text-bold italic font-racing mt-10 border-b-2 border-primary w-fit m-auto"
+      >
+        Last Race
+      </h2>
+      <h3
+        class="text-2xl md:text-4xl text-center text-bold italic font-racing m-10"
+      >
+        {{ lastRace.raceName }}
+      </h3>
+      <race-results-table :results="lastRace" sessionType="race" />
+    </div>
+  </main>
 </template>
 
 <script setup>
