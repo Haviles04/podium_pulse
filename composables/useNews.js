6a4450a5631d9e49d814bce3432978e75b2f01bb
Henry Aviles <haviles04@gmail.com>
2023-09-26 13:21:40 -0400

    fix: updated useNews

--- composables/useNews.js
+++ composables/useNews.js
@@ -1,7 +1,5 @@
 const config = useRuntimeConfig();
 const yesterday = new Date(Date.now() - 86400000).toLocaleDateString();
-
-console.log('fetching');
 const { data: news } = await useFetch(
   `https://newsapi.org/v2/everything?language=en&apiKey=${config.public.newsApiKey}&q=F1 Race&from=${yesterday}&domains=autosport.com, bbc.co.uk, sbnation.com, skysports.com`,
   {
