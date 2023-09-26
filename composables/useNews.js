9f20886043f27149c29342e602f73a9930bfc9e3
Henry Aviles <haviles04@gmail.com>
2023-09-26 13:21:27 -0400

    fix: updated useNews

--- composables/useNews.js
+++ composables/useNews.js
@@ -1,17 +1,16 @@
-export const useNews = async () => {
-  const news = ref(null);
-  const config = useRuntimeConfig();
-  const yesterday = new Date(Date.now() - 86400000).toLocaleDateString();
-  if (!news.value) {
-    const { data } = await useFetch(
-      `https://newsapi.org/v2/everything?language=en&apiKey=${config.public.newsApiKey}&q=F1 Race&from=${yesterday}&domains=autosport.com, bbc.co.uk, sbnation.com, skysports.com`,
-      {
-        transform: (data) => {
-          return data.articles.filter(({ title }) => title !== '[Removed]').slice(0, 5);
-        },
-      },
-    );
-    news.value = data.value;
-  }
-  return { news };
-};
+const config = useRuntimeConfig();
+const yesterday = new Date(Date.now() - 86400000).toLocaleDateString();
+
+console.log('fetching');
+const { data: news } = await useFetch(
+  `https://newsapi.org/v2/everything?language=en&apiKey=${config.public.newsApiKey}&q=F1 Race&from=${yesterday}&domains=autosport.com, bbc.co.uk, sbnation.com, skysports.com`,
+  {
+    transform: (data) => {
+      return data.articles.filter(({ title }) => title !== '[Removed]').slice(0, 5);
+    },
+  },
+);
+
+export default () => ({
+  news,
+});
