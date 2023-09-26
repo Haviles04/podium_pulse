570079abce5375c5deac2768d06d8eac577027da
Henry Aviles <haviles04@gmail.com>
2023-09-26 13:31:55 -0400

    fix: fixed useNews.js

--- composables/useNews.js
+++ composables/useNews.js
@@ -1,14 +1,14 @@
-const config = useRuntimeConfig();
-const yesterday = new Date(Date.now() - 86400000).toLocaleDateString();
-const { data: news } = await useFetch(
-  `https://newsapi.org/v2/everything?language=en&apiKey=${config.public.newsApiKey}&q=F1 Race&from=${yesterday}&domains=autosport.com, bbc.co.uk, sbnation.com, skysports.com`,
-  {
-    transform: (data) => {
-      return data.articles.filter(({ title }) => title !== '[Removed]').slice(0, 5);
-    },
-  },
-);
+export const useNews = async () => {
+  const config = useRuntimeConfig();
+  const yesterday = new Date(Date.now() - 86400000).toLocaleDateString();
 
-export default () => ({
-  news,
-});
+  const { data: news } = await useFetch(
+    `https://newsapi.org/v2/everything?language=en&apiKey=${config.public.newsApiKey}&q=F1 Race&from=${yesterday}&domains=autosport.com, bbc.co.uk, sbnation.com, skysports.com`,
+    {
+      transform: (data) => {
+        return data.articles.filter(({ title }) => title !== '[Removed]').slice(0, 5);
+      },
+    },
+  );
+  return { news };
+};
