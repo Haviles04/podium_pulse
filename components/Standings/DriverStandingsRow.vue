1b256e440656fb1a2f682f597f47ee1183cf582f
Henry Aviles <haviles04@gmail.com>
2023-09-26 10:32:18 -0400

    feat: added driver standings

---
+++ components/Standings/DriverStandingsRow.vue
@@ -0,0 +1,23 @@
+<template>
+  <tr>
+    <td class="p-2">{{ position }}</td>
+    <td class="hover:text-primary">
+      <nuxt-link :to="`/drivers/${Driver.driverId}`">{{
+        Driver.givenName + ' ' + Driver.familyName
+      }}</nuxt-link>
+      <img
+        class="ml-2 hidden md:inline"
+        :src="`https://flagsapi.com/${countryCode}/flat/24.png`"
+        :alt="`${driver.Driver.nationality} flag`"
+      />
+    </td>
+    <td>{{ points }}</td>
+  </tr>
+</template>
+
+<script setup>
+const { driver } = defineProps(['driver']);
+const { position, Driver, points } = driver;
+
+const countryCode = getCountryCode(Driver.nationality);
+</script>
--- pages/standings.vue
+++ pages/standings.vue
@@ -1,3 +1,13 @@
 <template>
-  <main><h2 class="font-italic mt-10 text-center font-racing text-4xl">Standings</h2></main>
+  <main>
+    <section class="m-auto mt-10 rounded-xl bg-secondary pb-10 md:max-w-[600px] md:p-10">
+      <h2 class="font-italic pt-2 text-center font-racing text-4xl">Standings</h2>
+
+      <driver-standings-table v-if="championship === 'WDC'" />
+    </section>
+  </main>
 </template>
+
+<script setup>
+const championship = ref('WDC');
+</script>
