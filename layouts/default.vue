96b2b836bdbe84ba1c20cbaecdef955541d7ebf2
Henry Aviles <haviles04@gmail.com>
2023-09-27 11:04:47 -0400

    feat: added nuxt loading indicator

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <nav class="border-b-4 border-primary bg-secondary">
+    <NuxtLoadingIndicator />
     <div class="m-auto flex h-full max-w-[1000px] items-center justify-between">
       <nuxt-link to="/" class="mx-6">Podium Pulse</nuxt-link>
       <desktop-nav class="hidden md:flex" />
