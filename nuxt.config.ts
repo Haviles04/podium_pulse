bb19f81f0d3c9a5a9014d1da8ba5a1d36a3df52d
Henry Aviles <haviles04@gmail.com>
2023-10-29 12:59:16 -0400

    fix: updated icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,8 +9,53 @@ export default defineNuxtConfig({
       description: 'Formula 1 information app',
       icons: [
         {
-          src: '/race.png',
+          src: '/icons/race-72.png',
+          sizes: '72x72',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-96.png',
+          sizes: '96x96',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-120.png',
+          sizes: '120x120',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-128.png',
+          sizes: '128x128',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-144.png',
           sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-152.png',
+          sizes: '152x152',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-180.png',
+          sizes: '180x180',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-384.png',
+          sizes: '384x384',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/race-512.png',
+          sizes: '512x512',
           type: 'image/png',
         },
       ],
