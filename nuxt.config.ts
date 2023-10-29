ecebf2e9c1f9272603e112d879eb4ad2e384afbb
Henry Aviles <haviles04@gmail.com>
2023-10-29 08:59:01 -0400

    feat: added pwa support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@vite-pwa/nuxt', '@nuxtjs/tailwindcss'],
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
   },
